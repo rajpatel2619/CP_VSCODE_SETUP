ad38ac239242556d17d7a4ae7682608784af5a9c
raj patel <kakhilesh79@gmail.com>
2021-06-08 09:32:44 +0530

    changes

--- Current/1.cpp
+++ Current/1.cpp
@@ -1,54 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long ll;typedef long double ld;typedef pair<int,int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
 
 
+int minEdgeBFS(vector <int> edges[], int u, int v, int n)
+{
+	vector<bool> visited(n, 0);
 
-void solve(){
-    int n , m;
-    cin>>n>>m;
-    int a[n];
-    int b[m];
-    for(int i=0;i<n;i++) cin>>a[i];
-    for(int i=0;i<m;i++) cin>>b[i];
+	vector<int> distance(n, 0);
+	queue <int> Q;
+	distance[u] = 0;
 
-    for(int i=0;i<m;i++){
-        if(b[i]==1){
-            cout<<0<<" ";
-            continue;
-        }else{
+	Q.push(u);
+	visited[u] = true;
+	while (!Q.empty())
+	{
+		int x = Q.front();
+		Q.pop();
 
-        int ts = b[i]-1;
-        int jt=0,kt=0;
-        for(int j=ts,k=ts;j>=0||k<n;j--,k++){
-            if(j>=0 && a[j] == 1){
-                jt = ts - j ; 
-                break;
-            }
-            else if(k<n && a[k] == 2){
-                 kt = k-ts;
-                 break;
-            }
-        }
-        int time=-1;
-        if(jt> 0 && kt > 0 ) time = (jt < kt) ? jt : kt;
-        else if(jt > 0 && kt == 0) time =jt;
-        else if(kt > 0 && jt== 0) time=kt;
-        cout<<time<<" ";
-        }
-    }
-    cout<<"\n";
+		for (int i=0; i<edges[x].size(); i++)
+		{
+			if (visited[edges[x][i]])
+				continue;
 
+			// update distance for i
+			distance[edges[x][i]] = distance[x] + 1;
+			Q.push(edges[x][i]);
+			visited[edges[x][i]] = 1;
+		}
+	}
+	return distance[v];
 }
 
+// function for addition of edge
+void addEdge(vector <int> edges[], int u, int v)
+{
+edges[u].push_back(v);
+edges[v].push_back(u);
+}
+
+
+void solve()
+{
+    int n,q;
+    cin>>n>>q;
+	vector <int> edges[n*n];
+    for(int i=1;i<n;i++){
+        int u,v;
+        cin>>u>>v;
+	    addEdge(edges, u, v);
+    }
+
+    for(int i=0;i<q;i++){
+        int a,b;
+        cin>>a>>b;
+        int sum=0;
+        for(int j=1;j<=n;j++){
+            
+            sum+=min(minEdgeBFS(edges,j,a,n),minEdgeBFS(edges,j,b,n));
+        }
+        cout<<sum<<"\n";
+    }
+    // minEdgeBFS(edges, u, v, n);
+	
+}
 
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); 
-    int t ;
+    int t;
     cin >> t;
     while(t--){
     	solve();
     }
     return 0;
 }
+
